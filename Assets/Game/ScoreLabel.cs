17ac040d4d9de724c1f8b39c8bbf5f78f8ed2be7
Tomáš Malý <tomas.maly@melown.com>
2019-08-27 13:49:10 +0200

    score rate

--- Assets/Game/ScoreLabel.cs
+++ Assets/Game/ScoreLabel.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class ScoreLabel : MonoBehaviour
 {
-    private UnityEngine.UI.Text text;
+    public UnityEngine.UI.Text textScore;
+    public UnityEngine.UI.Text textRate;
+
+    private System.DateTime startTime;
+    private int bestRate = 0;
 
     void Start()
     {
-        text = GetComponent<UnityEngine.UI.Text>();
+        startTime = System.DateTime.Now;
     }
 
     void Update()
     {
         if (Input.GetAxis("Restart") > 0)
+        {
             ZombieController.killCount = 0;
-        text.text = ZombieController.killCount.ToString();
+            startTime = System.DateTime.Now;
+        }
+        textScore.text = ZombieController.killCount.ToString();
+        double duration = (System.DateTime.Now - startTime).TotalSeconds;
+        int rate = Mathf.RoundToInt((float)(60d * ZombieController.killCount / duration));
+        if (ZombieController.killCount >= 10 && rate > bestRate)
+            bestRate = rate;
+        textRate.text = rate.ToString() + " (" + bestRate.ToString() + ")";
     }
 }
